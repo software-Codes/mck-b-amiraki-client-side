82dddbf3ddf78897a93738bd09b4b1937474a054
CollinsMunene-Developer <collinsentrepreneur@gmail.com>
2025-02-02 13:10:54 +0000

    remove unused hooks and components; add Home screen and new icons

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,32 +1,19 @@
-import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import { Link, Stack, } from 'expo-router';
+import { Text, View } from 'react-native';
+import React from 'react';
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 
 export default function NotFoundScreen() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
+      <Stack.Screen  options={{ title: 'Oops!' }}  />
+      <View className=' bg-slate-700 h-full items-center justify-center  flex'  >
+        <Text className='text-red-500 text-2xl' >Sorry, This screen doesn' exist.</Text>
+        <Link href="/" >
+          <Text className='text-white text-2xl' >Go to home screen!</Text>
         </Link>
-      </ThemedView>
+      </View>
     </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-});
