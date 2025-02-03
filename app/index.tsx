94cb51e022bbafed3c6fc2430300993e82e3917d
CollinsMunene-Developer <collinsentrepreneur@gmail.com>
2025-02-03 12:25:00 +0300

    add font config in the layout and configure with tailwind.json

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,13 @@
-import { View, Text } from "react-native"
-
-const Home =  ( ) => {
-    return(
-        <View>
-            <Text>Home</Text>
-            
-
-        </View>
+import {SafeAreaView} from "react-native-safe-area-context";
+import {ScrollView, Text, View} from "react-native";
+export  default  function HomeScreen (){
+    return (
+        <SafeAreaView className="h-full items-center flex-1 justify-center mt-12 " >
+            <ScrollView className=" " >
+                <Text className="text-3xl" >
+                    Home Screen
+                </Text>
+            </ScrollView>
+        </SafeAreaView>
     )
-
 }
