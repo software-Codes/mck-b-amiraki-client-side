519ecc4d4a8d04fb71d66f216ad8b3ef5badb5ce
CollinsMunene-Developer <collinsentrepreneur@gmail.com>
2025-02-03 18:17:18 +0300

    feat: add onboarding screen with swiper and constants for images and icons

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,13 +1,42 @@
-import {SafeAreaView} from "react-native-safe-area-context";
-import {ScrollView, Text, View} from "react-native";
-const  Onboarding = () =>{
-    return (
-        <SafeAreaView className="h-full items-center flex-1 justify-center mt-12 " >
-                <Text className="text-3xl" >
-                   On boarding screen
-                </Text>
-        </SafeAreaView>
-    )
-}
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { router } from "expo-router";
+import { useRef, useState } from "react";
+import Swiper from "react-native-swiper";
+import { onboarding } from "@/constants";
+
+const Onboarding = () => {
+  const swiperRef = useRef<Swiper>(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+  return (
+    <SafeAreaView className="flex h-full items-center justify-between bg-white ">
+      <TouchableOpacity
+        onPress={() => {
+          router.replace("/(auth)/sign-in");
+        }}
+        className="w-full justify-end items-end p-5"
+      >
+        <Text className="text-black text-md font-jakartaBold">Skip</Text>
+      </TouchableOpacity>
+      <Swiper
+        ref={swiperRef}
+        loop={false}
+        dot={
+          <View className=" w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full " />
+        }
+        activeDot={
+          <View className=" w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full  " />
+        }
+        onIndexChanged={(index) => setActiveIndex(index)}
+      >
+        {onboarding.map((item) => (
+          <View className="">
+            <Text> {item.title}</Text>
+          </View>
+        ))}
+      </Swiper>
+    </SafeAreaView>
+  );
+};
 
 export default Onboarding;
